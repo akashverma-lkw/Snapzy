29548d794085f047686a3e6349a4536b48e52d53
Akash Verma <akashvermastp7@gmail.com>
2025-02-24 12:21:38 +0530

    Update Post Component

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -125,7 +125,7 @@ const Post = ({ post }) => {
 
 	return (
 		<>
-			<div className='flex gap-2 mr-32 items-start p-4 border-b border-gray-700'>
+			<div className='flex gap-2 mr-32 items-start p-4 border-b border-gray-700 w-full max-w-2xl mx-auto'>
 				<div className='avatar'>
 					<Link to={`/profile/${postOwner.username}`} className='w-8 rounded-full overflow-hidden'>
 						<img src={postOwner.profileImg || "/avatar-placeholder.png"} />
